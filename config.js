5f8baa72c525da5f599291d06e81848f85c787fd
traitmods <112393469+traitmods@users.noreply.github.com>
2023-09-23 21:12:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -22,23 +22,23 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
+global.botname = "ERICSTAR BOT" 
+global.author = "TRAITMODS"  
+global.packname = "ERICSTAR BOT"  
 global.myweb = "Enter Your Any Website Profile Link Here"
-global.footer = "©\t" + "Enter Your Watermark Here"
-global.prefa = ['-'] // single prefix
+global.footer = "©\t" + "ERICSTAR"
+global.prefa = ['.'] // single prefix
 global.themeemoji = "🎐"
 global.autoBio = "$mode : $time ⌚ || $date 🗓️ || $botname" // Leave it as it is if you didn't understood!
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
+global.ownername = "ERICSTAR"    
+global.ownernumber = ["2349155556447"]
 global.ytname = "YT: Enter Your Channel Name Here"
-global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
+global.continent = "AFRICA" // your continent name 
+global.region = "NIGERIA"
 global.state = "Enter Your State Name Here" // your state name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
 global.instagramId = "Enter Your Insta ID Here"
